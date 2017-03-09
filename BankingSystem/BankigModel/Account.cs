e82a26905677ba91da31ff71c926d34f3d6753cb
Clase <Clase>
2017-03-08 19:31:00 -0500

    Creación de Proyecto Base

---
+++ BankingSystem/BankigModel/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankigModel
+{
+    public class Account
+    {
+        private string _name;
+
+        public string getName()
+        {
+            return _name;
+        }
+
+        public void setName(string name) 
+        {
+            _name = name;
+        }
+    }
+}
